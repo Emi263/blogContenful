7d8e09f14ec664d806ac8aa0a4005da66923fdf1
Emi <emi.ramilli12345@gmail.com>
2021-09-23 18:28:45 +0200

    final commit

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ import Pagination from "../components/Pagination";
 import { useState } from "react";
 export async function getStaticProps() {
   const client = createClient({
-    space: process.env.NEXT_PUBLIC_SPACE,
-    accessToken: process.env.NEXT_PUBLIC_TOKEN,
+    space: process.env.NEXT_PUBLIC_VERCEL_SPACE,
+    accessToken: process.env.NEXT_VERCEL_PUBLIC_TOKEN,
   });
 
   const response = await client.getEntries({ content_type: "blog" });
